7c2ad7c580d0c3eba47f29e2630eb03f673cf9d0
RyanDaCoder <ryancollins555@gmail.com>
2020-12-05 14:01:10 -0800

    added a spritesheet

---
+++ spritesheet.js
@@ -0,0 +1,23 @@
+class SpriteSheet {
+    constructor (src, width, height, states = {}, newWidth = width, newHeight = height) {
+        this.img = new Image ();
+        this.img.src = src;
+
+        this.states = states;
+        this.width = width;
+        this.height = height;
+        this.newWidth = newWidth;
+        this.newHeight = newHeight;
+
+    }
+    draw (ctx, x, y, t = Date.now()) {
+        if (this.state === undefined) {
+            return
+        }
+        
+        var [row, numberFrames, frameTime] = this.states[this.state]
+        var index = Math.floor(t % (numberFrames * frameTime) / frameTime);
+        
+        ctx.drawImage(this.img, index * this.width, row * this.height, this.width, this.height, x, y, this.newWidth, this.newHeight);
+    }
+}
